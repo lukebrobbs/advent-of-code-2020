a6a3d576b086808410ffb6697ff3a47800c7ccfb
Luke Brobbin <luke.brobbin@truepill.com>
2020-12-02 21:15:30 +0000

    remove extra loop

--- day-2.go
+++ day-2.go
@@ -9,12 +9,6 @@ import (
 )
 
 var inputFile = flag.String("inputFile", "inputs/day-2.input", "Relative file path")
-var partB = flag.Bool("partB", false, "Whether to us the part B logic")
-
-type password struct {
-	amountFrom, amountTo int
-	letter, password     string
-}
 
 func main() {
 	flag.Parse()
@@ -24,44 +18,32 @@ func main() {
 	}
 	contents := string(bytes)
 	split := strings.Split(contents, "\n")
-	passwords := make([]password, len(split))
-	for i, s := range split {
+	var totalValid, totalValidB int
+
+	for _, s := range split {
 		splitSeen := strings.Split(s, " ")
 		amounts := strings.Split(splitSeen[0], "-")
 		from, err := strconv.Atoi(amounts[0])
-		if err != nil {
-			fmt.Printf("Failed to parse %s\n", s)
-			break
-		}
 		to, err := strconv.Atoi(amounts[1])
 		if err != nil {
 			fmt.Printf("Failed to parse %s\n", s)
 			break
 		}
-		passwords[i] = password{
-			amountFrom: from,
-			amountTo:   to,
-			letter:     strings.TrimSuffix(splitSeen[1], ":"),
-			password:   splitSeen[2],
-		}
-	}
-	var totalValid int
-	for _, p := range passwords {
-		a := strings.Count(p.password, p.letter)
-		if a >= p.amountFrom && a <= p.amountTo {
+		letter := strings.TrimSuffix(splitSeen[1], ":")
+		password := splitSeen[2]
+
+		t := strings.Count(password, letter)
+		if t >= from && t <= to {
 			totalValid++
+		}
+
+		a := string(password[from-1])
+		b := string(password[to-1])
+		if a != b && (a == letter || b == letter) {
+			totalValidB++
 		}
 	}
 	fmt.Println(totalValid)
-	if *partB {
-		var totalValidB int
-		for _, p := range passwords {
-			a := string(p.password[p.amountFrom-1])
-			b := string(p.password[p.amountTo-1])
-			if a != b && (a == p.letter || b == p.letter) {
-				totalValidB++
-			}
-		}
-		fmt.Println(totalValidB)
-	}
+	fmt.Println(totalValidB)
+
 }
